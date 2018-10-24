ede6ec20d9bd0f33e4b996f7a68a1e9044afe193
jacobsilver2 <jacobsilver2@mac.com>
2018-10-24 09:10:26 -0400

    start ch 15

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+    //     return items
+    // }
+
+    // super easy if you don't need to
+    // run extra logic or check for auth
+    items: forwardTo('db')
+
+};
 
 module.exports = Query;
