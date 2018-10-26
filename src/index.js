d88b408ba5f06e9dbeee62839e6ee1989594b4fb
jacobsilver2 <jacobsilver2@mac.com>
2018-10-26 10:55:50 -0400

    added query to check if user is signed in

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@
 const cookieParser = require('cookie-parser');
 require('dotenv').config({path: 'variables.env'});
 const createServer = require('./createServer');
+const jwt = require('jsonwebtoken');
 const db = require('./db');
 
 const server = createServer();
 
 //Use Express middleware to handle cookies
 server.express.use(cookieParser());
-//Todo Use Express to populate current user
+
+//decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+    const {token} = req.cookies;
+    if(token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        //put the user id onto the request
+        req.userId = userId;
+    }
+    next();
+});
+
 
 server.start({
     cors: {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -8,10 +8,20 @@ const Query = {
 
     // super easy if you don't need to
     // run extra logic or check for auth
+
+
     items: forwardTo('db'),
     item: forwardTo('db'),
-    itemsConnection: forwardTo('db')
-
+    itemsConnection: forwardTo('db'),
+    me(parent, args, ctx, info) {
+        //check if there is a current user ID
+        if(!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user({
+            where: {id: ctx.request.userId},
+        }, info);
+    }
 };
 
 module.exports = Query;
